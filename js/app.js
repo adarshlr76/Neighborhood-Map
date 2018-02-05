c83145e07279c113c4b0222d84a32b34690b7e39
Adarsh <adarshlr76@gmail.com>
2018-02-05 11:25:33 +0530

    refactored the code - need to add marker

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 
     var map;
     var markers = [];
+    var bounds;
 
 	var all_locations = [
 		{place : 'Discovery Docks Apartments'},
@@ -16,65 +17,24 @@
 		{place : 'Pizza Express'}
 	];
 
-     function initMap() {
-     	        // Create a styles array to use with the map.
+function initMap() {
+   	bounds = new google.maps.LatLngBounds();
+
+    // Create a styles array to use with the map.
     
        
-        var uluru = {lat: 40.7413549, lng: -73.99802439999996};
-        map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 13,
-          center: uluru,
+    var uluru = {lat: 40.7413549, lng: -73.99802439999996};
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: uluru,
       
-          title:"Google office"
+        title:"Google office"
 
-        });
-        var geocoder = new google.maps.Geocoder();
+    });
 
-        // The following group uses the location array to create an array of markers on initialize.
-        for (var i = 0; i < all_locations.length; i++) {
-          // Get the position from the location array.
-          var position =  geocodeAddress(all_locations[i].place, geocoder);
-
-          var title = all_locations[i].place;
-          // Create a marker per location, and put into markers array.
-          var marker = new google.maps.Marker({
-            position: position,
-            title: title,
-            animation: google.maps.Animation.DROP,
-            //icon: defaultIcon,
-            id: i
-          });
-          // Push the marker to our array of markers.
-          markers.push(marker);
-/*          // Create an onclick event to open the large infowindow at each marker.
-          marker.addListener('click', function() {
-            populateInfoWindow(this, largeInfowindow);
-          });
-          // Two event listeners - one for mouseover, one for mouseout,
-          // to change the colors back and forth.
-          marker.addListener('mouseover', function() {
-            this.setIcon(highlightedIcon);
-          });
-          marker.addListener('mouseout', function() {
-            this.setIcon(defaultIcon);
-          });*/
-        }
+}
 
 
-        
-
-     }
-
-      function geocodeAddress(address, geocoder) {
-        //var address = document.getElementById('address').value;
-        geocoder.geocode({'address': address}, function(results, status) {
-          if (status === google.maps.GeocoderStatus.OK) {
-            return results[0].geometry.location;
-          } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-          }
-        });
-      }     
 
 var Location = function(data) {
 
@@ -96,9 +56,10 @@ var ViewModel = function() {
 	
 	this.currentLocation = ko.observable(this.placesList()[0]);
 
-	this.selectedLocation = function(selectedPlace){
+	
+	/*this.selectedLocation = function(selectedPlace){
 		self.currentLocation(selectedPlace);
-	}
+	}*/
 
 }
 ko.applyBindings(new ViewModel());
