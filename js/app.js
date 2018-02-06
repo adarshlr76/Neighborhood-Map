a686bec112e65f56437b87bf8de7dfe2c34c4d3b
Adarsh <adarshlr76@gmail.com>
2018-02-06 11:20:36 +0530

    Added animations on click of marker or selection of list item

--- js/app.js
+++ js/app.js
@@ -34,69 +34,14 @@ var baseUrl = "https://api.foursquare.com/v2/venues/",
     fsClient_id = "client_id=0W2YAEP0XHAE554XVKJQFI0TRCKI2DZMJR5L5VHZ1ZGZCWMP",
     fsClient_secret = "&client_secret=3PDIQU1WQEZKOODKMP0KUJRJZE3A0KPRLAZ35P5UJL05ECL2",
 	fsVersion = "&v=20161507";
-function initMap() {
-    // Constructor creates a new map - only center and zoom are required.
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 40.7413549, lng: -73.9980244},
-        zoom: 13
-    });
-
-
-    var bounds = new google.maps.LatLngBounds();
-    var contentString;
-    var largeInfowindow;
-    // The following group uses the location array to create an array of markers on initialize.
-    for (var i = 0; i < all_locations.length; i++) {
-        // Get the position from the location array.
-        var position = all_locations[i].location;
-        var title = all_locations[i].place;
-        // Create a marker per location, and put into markers array.
-        var marker = new google.maps.Marker({
-            map: map,
-            position: position,
-            title: title,
-            animation: google.maps.Animation.DROP,
-            id: i
-        });
-        
-
-        largeInfowindow = new google.maps.InfoWindow();
-        // Push the marker to our array of markers.
-        markers.push(marker);
-        // Create an onclick event to open an infowindow at each marker.
-        marker.addListener('click', function() {
-            populateInfoWindow(this, largeInfowindow);
-        });
-          bounds.extend(markers[i].position);
-    }
-        // Extend the boundaries of the map for each marker
-    map.fitBounds(bounds);
-}
 
 
 
-// This function populates the infowindow when the marker is clicked. We'll only allow
-// one infowindow which will open at the marker that is clicked, and populate based
-// on that markers position.
-function populateInfoWindow(marker, infowindow) {
-    // Check to make sure the infowindow is not already opened on this marker.
-    if (infowindow.marker != marker) {
-
-        infowindow.marker = marker;
-        infowindow.setContent('<div id="info-window-content">' + marker.title + '</div>');
-        infowindow.open(map, marker);
-        // Make sure the marker property is cleared if the infowindow is closed.
-        infowindow.addListener('closeclick',function(){
-        	infowindow.setMarker = null;
-        });
-
-    }
-}
 
 var Location = function(data) {
 
 	this.place = ko.observable(data.place);
-	this.visible = ko.observable(true);
+	//this.visible = ko.observable(true);
 	this.markerId = ko.observable(0);
 
 	/*this.showMarker = ko.computed(function() {
@@ -123,17 +68,17 @@ var ViewModel = function() {
 		var filter = self.searchTerm().toLowerCase();
 		if (!filter) {
 			self.placesList().forEach(function(locationItem){
-				locationItem.visible(true);
+				//locationItem.visible(true);
 				markers[locationItem.markerId()].setMap(map);
 			});
 			return self.placesList();
 		} else {
 			return ko.utils.arrayFilter(self.placesList(), function(locationItem) {
-				console.log(locationItem.place());
+				//console.log(locationItem.place());
 
 				var string = locationItem.place().toLowerCase();
 				var result = (string.search(filter) >= 0);
-				locationItem.visible(result);
+				//locationItem.visible(result);
 				if(result === true) {
 					markers[locationItem.markerId()].setMap(map);
 				} else {
@@ -159,6 +104,70 @@ var ViewModel = function() {
 		//this.markerId(this.count() + 1);
 	
 	}
+
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+	this.populateInfoWindow = function (marker, infowindow) {
+		marker.setAnimation(google.maps.Animation.BOUNCE);
+      	setTimeout(function() {
+      		marker.setAnimation(null);
+     	}, 2100);
+
+	    // Check to make sure the infowindow is not already opened on this marker.
+	    if (infowindow.marker != marker) {
+
+	        infowindow.marker = marker;
+	        infowindow.setContent('<div id="info-window-content">' + marker.title + '</div>');
+	        infowindow.open(map, marker);
+	        // Make sure the marker property is cleared if the infowindow is closed.
+	        infowindow.addListener('closeclick',function(){
+	        	infowindow.setMarker = null;
+	        });
+
+	    }
+	}
+	this.initMap = function() {
+	    // Constructor creates a new map - only center and zoom are required.
+	    map = new google.maps.Map(document.getElementById('map'), {
+	        center: {lat: 40.7413549, lng: -73.9980244},
+	        zoom: 13
+	    });
+
+
+	    var bounds = new google.maps.LatLngBounds();
+	    var contentString;
+	    var largeInfowindow;
+	    // The following group uses the location array to create an array of markers on initialize.
+	    for (var i = 0; i < all_locations.length; i++) {
+	        // Get the position from the location array.
+	        var position = all_locations[i].location;
+	        var title = all_locations[i].place;
+	        // Create a marker per location, and put into markers array.
+	        var marker = new google.maps.Marker({
+	            map: map,
+	            position: position,
+	            title: title,
+	            animation: google.maps.Animation.DROP,
+	            id: i
+	        });
+	        
+
+	        largeInfowindow = new google.maps.InfoWindow();
+	        // Push the marker to our array of markers.
+	        markers.push(marker);
+	        // Create an onclick event to open an infowindow at each marker.
+	        marker.addListener('click', function() {
+	            self.populateInfoWindow(this, largeInfowindow);
+
+
+	        });
+	          bounds.extend(markers[i].position);
+	    }
+	        // Extend the boundaries of the map for each marker
+	    map.fitBounds(bounds);
+	}
+
 	// Creating click for the list item
     
 
@@ -170,6 +179,7 @@ var ViewModel = function() {
 
 
 	}
+	this.initMap();
 	//$("#info-window-content").html("Hello World");
 
 
@@ -209,4 +219,6 @@ var ViewModel = function() {
 */
 
 }
-ko.applyBindings(new ViewModel());
+function start(){
+	ko.applyBindings(new ViewModel());
+}
